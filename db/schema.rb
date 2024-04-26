2e3edaadb24c69edfaa0c697f611a42e717d4108
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-04-26 16:41:46 -0300

    Atualiza o tipo de products price para decimal

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_26_193645) do
   create_table "products", force: :cascade do |t|
     t.integer "store_id", null: false
     t.string "title"
-    t.string "price"
+    t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["store_id"], name: "index_products_on_store_id"
