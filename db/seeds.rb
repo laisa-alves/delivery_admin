600108631c7660ccb1005847b174bc02b4c23bfd
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-04-28 09:20:55 -0300

    Atualiza os seeds para diferentes user roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,23 @@ user = User.find_by(email: "store@example.com")
 
 if !user
   user = User.new(
-    email: "store@example.com",
+    email: "admin@example.com",
     password: "123456",
-    password_confirmation: "123456"
+    password_confirmation: "123456",
+    role: :admin
   )
   user.save!
 end
 
 [ "Spice Grill", "Smokestack Box" ].each do |store|
+  user = User.new(
+    email: "#{store.split.map { |s| s.downcase }.join(".")}@example.com",
+    password: "123456",
+    password_confirmation: "123456",
+    role: :seller
+  )
+  user.save!
+
   Store.find_or_create_by!(name: store, user: user)
 end
 
