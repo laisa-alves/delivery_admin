e2bb316e1160868b72122c6d7353116abe599d4f
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-04-28 10:45:48 -0300

    Atualiza a root view e cria redirecionamento para usuários não autorizados

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
 
   def listing
     if !current_user.admin?
-      redirect_to welcome_path
+      redirect_to root_path, notice: "No permission for you"
     end
 
     @products = Product.includes(:store)
