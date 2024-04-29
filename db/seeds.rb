ab4d3d18fb2c287f268fc9c742b777710bdf87e7
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-04-29 07:20:06 -0300

    Adiciona buyers no seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
-user = User.find_by(email: "store@example.com")
+# Cria ou retorna o admin
+admin = User.find_by(email: "admin@example.com")
 
-if !user
-  user = User.new(
+if !admin
+  admin = User.new(
     email: "admin@example.com",
     password: "123456",
     password_confirmation: "123456",
     role: :admin
   )
-  user.save!
+  admin.save!
 end
 
+# Cria novas lojas
 [ "Spice Grill", "Smokestack Box" ].each do |store|
   user = User.new(
     email: "#{store.split.map { |s| s.downcase }.join(".")}@example.com",
@@ -22,6 +24,7 @@ end
   Store.find_or_create_by!(name: store, user: user)
 end
 
+# Cria produtos para as lojas anteriores
 ["Scotch Eggs", "Chicken Parm", "Carbonara", "Kebab", "Fish and Chips"].each do |dish|
   store = Store.find_by(name: "Spice Grill")
   Product.find_or_create_by!(title: dish, store: store)
@@ -30,4 +33,19 @@ end
 ["Mushroom Risotto", "Caesar Salad", "Tuna Sashimi", "Chicken Milanese"].each do |dish|
   store = Store.find_by(name: "Smokestack Box")
   Product.find_or_create_by!(title: dish, store: store)
+end
+
+# Cria usuários do tipo buyer
+["Aracelis Weissnat", "Lorenza Upton", "Arlen Brown"].each do |buyer|
+  email = buyer.split.map { |s| s.downcase }.join(".")
+  user = User.find_by(email: email)
+  if !user
+    user = User.new(
+      email: "#{email}@example.com",
+      password: "123456",
+      password_confirmation: "123456",
+      role: "buyer"
+    )
+    user.save!
+  end
 end
