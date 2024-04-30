bd816422962785eec69556a9a58f9927a43d592d
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-04-30 16:09:21 -0300

    Corrige o teste de /stores para realizar o sign_in pela API, com uma credencial, recuperar o token de validação e passar esse token ao solicitar uma rota de stores/id

--- spec/requests/api/stores_spec.rb
+++ spec/requests/api/stores_spec.rb
@@ -2,19 +2,48 @@ require "rails_helper"
 
 RSpec.describe "/stores", type: :request do
   let(:user) {
-    user = User.new(email: "user_01@example.com", password: "123456", password_confirmation: "123456")
+    user = User.new(
+      email: "user_01@example.com",
+      password: "123456",
+      password_confirmation: "123456",
+      role: :seller
+      )
     user.save!
     user
   }
 
-  before {
-    sign_in(user)
-  }
+  let(:credential) { Credential.create_access(:seller) }
+
+  def api_sign_in(user, credential)
+    post(
+      "/sign_in",
+      headers: {
+        "Accept" => "application/json",
+        "X-API-KEY" => credential.key
+      },
+      params: {
+        signin: {
+          email: user.email,
+          password: user.password
+        }
+      }
+    )
+    JSON.parse(response.body)
+  end
+
+  # Na primeira execução do let a função é chamada e seu retorno armazenado no let. A partir disso as próximas invocações já retornam direto para o resultado da primeira execução, sem precisar resolver a função em todas as invocações.
+  let(:signed_in) { api_sign_in(user, credential) }
 
   describe "GET /show" do
     it "renders a successful response with stores data" do
       store = Store.create! name: "New Store", user: user
-      get "/stores/#{store.id}", headers: {"Accept" => "application/json"}
+      get(
+        "/stores/#{store.id}",
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Bearer #{signed_in["token"]}"
+        }
+        )
       json = JSON.parse(response.body)
 
       expect(json["name"]).to eq store.name
