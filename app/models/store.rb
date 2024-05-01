490e78793eb6a6aeccf890527d92aaad80cb0a8d
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-01 08:29:47 -0300

    Adiciona before_validation callback para garantir que o vendedor seja definido como nil se o usuário existir e não for um seller

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,12 @@
 class Store < ApplicationRecord
   belongs_to :user
   has_many :products
+  before_validation :ensure_seller
   validates :name, presence: true, length: {minimum: 3}
+
+  private
+
+  def ensure_seller
+    self.user = nil if self.user && !self.user.seller?
+  end
 end
