5f7790e8636b1cd6362fa99a48564efd6087aa1c
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-22 09:56:16 -0300

    Adiciona coluna description na tabela de products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_10_191051) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_125410) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_10_191051) do
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["store_id"], name: "index_products_on_store_id"
   end
 
