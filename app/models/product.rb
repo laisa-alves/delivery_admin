fe1fe6b24428cb689e49cd6ebfc13b462218827d
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-22 15:00:18 -0300

    Adiciona validações para o model de produtos e a relação com imagem do ActiveStorage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :store
   has_many :orders, through: :order_items
+
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
+  validates :title, presence: true, length: { maximum: 55 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :description, length: { maximum: 200 }
 end
