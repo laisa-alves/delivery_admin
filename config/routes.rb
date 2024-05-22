a88f16fc57fd72cc3ac0801ea19101abad9ebc0b
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-22 18:56:32 -0300

    Cria rotas para o CRUD de products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :stores
+  
+  resources :stores do
+    resources :products
+  end
+
   get "listing" => "products#listing"
 
   post "new" => "registrations#create", as: :create_registration
