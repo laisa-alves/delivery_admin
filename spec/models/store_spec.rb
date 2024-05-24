4098d0be87e65d215c378e3fa9825dca12136c5a
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-24 08:47:42 -0300

    Adiciona testes no model de store

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -4,8 +4,24 @@ RSpec.describe Store, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_length_of(:name).is_at_least(3) }
-    it { should belong_to(:user) }
   end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:products) }
+    it { should have_one_attached(:image) }
+  end
+
+  describe 'image variants' do
+    let(:store) { create(:store) }
+
+    it 'allows image variants to be created' do
+      store.image.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'test_image.png')),
+                             filename: 'test_image.png', content_type: 'image/png')
+      expect(store.image.variant(resize_to_limit: [100, 100])).to be_present
+    end
+  end
+
 
   describe "belongs_to" do
     let(:seller) {
@@ -29,6 +45,11 @@ RSpec.describe Store, type: :model do
     it "should not belong to admin users" do
       store = Store.create(name: "Store name", user: admin)
       expect(store.errors.full_messages).to eq ["User must exist"]
+    end
+
+    it "should belong to seller users" do
+      store = Store.create(name: "Store name", user: seller)
+      expect(store.errors[:user]).to be_empty
     end
   end
 end
