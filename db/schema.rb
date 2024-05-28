00ba89363c5010a44e1e6d35a04a9a19abed99da
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-28 11:14:48 -0300

    Cria a migração para adicionar o discard na tabela de Stores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_22_125410) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_105734) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -82,6 +82,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_22_125410) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.datetime "discarded_at"
+    t.index ["discarded_at"], name: "index_stores_on_discarded_at"
     t.index ["user_id"], name: "index_stores_on_user_id"
   end
 
