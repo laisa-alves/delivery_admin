48c74b49235212bcf932080666b7f196ac99ba6d
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-29 21:26:33 -0300

    Cria migração da coluna active para lojas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_224453) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_183742) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -85,6 +85,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_224453) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.datetime "discarded_at"
+    t.boolean "active", default: true
     t.index ["discarded_at"], name: "index_stores_on_discarded_at"
     t.index ["user_id"], name: "index_stores_on_user_id"
   end
