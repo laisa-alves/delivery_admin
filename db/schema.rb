77c99bc970a974a4409e84a4ec4ef4f429980ed0
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-31 18:58:47 -0300

    Adiciona migração da coluna description para stores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_31_145519) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_31_214648) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_31_145519) do
     t.datetime "discarded_at"
     t.boolean "active", default: true
     t.integer "category"
+    t.text "description"
     t.index ["discarded_at"], name: "index_stores_on_discarded_at"
     t.index ["user_id"], name: "index_stores_on_user_id"
   end
