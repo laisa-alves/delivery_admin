b265dbc4b968f142fd80359f87f2080dd3052603
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-06-02 14:40:53 -0300

    Atualiza as factories de store e product para conter os novos requisitos obrigatórios

--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     association :store, factory: :store
     title { "Test Product" }
     price { 10.99 }
+    category { Product.categories.keys.sample }
+    description { "Lorem ipsum dolor sit amet, consectetur adipiscing elit." }
 
     after(:build) do |product|
       product.image.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'test_image.png')), filename: 'test_image.png', content_type: 'image/png')
--- spec/factories/store.rb
+++ spec/factories/store.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :store do
     association :user, factory: :user, role: :seller
-    name { "Test Store" }
+    sequence(:name) {|n| "Store #{n}"}
+    category { Store.categories.keys.sample }
+    description { "Lorem ipsum dolor sit amet, consectetur adipiscing elit." }
 
     after(:build) do |store|
       store.image.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'test_image.png')), filename: 'test_image.png', content_type: 'image/png')
