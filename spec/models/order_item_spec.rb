e0d2a7f24cff87db7ba2f2a7a9d07e651129ea76
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-06-06 16:07:34 -0300

    Refatora os testes de model para order_items

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -6,32 +6,12 @@ RSpec.describe OrderItem, type: :model do
     it { should belong_to(:product).required }
   end
 
-  let(:buyer) do
-    buyer = User.create!(
-      email: 'buyer@email.com',
-      password: '123456',
-      password_confirmation: '123456',
-      role: :buyer
-    )
-    buyer
-  end
-
-  let(:seller) do
-    seller = User.create!(
-      email: 'seller@email.com',
-      password: '123456',
-      password_confirmation: '123456',
-      role: :seller
-    )
-    seller
-  end
-
-  let(:store) do
-    Store.create!(name: 'Blue Plate', user: seller)
-  end
+  let(:buyer) { create(:user, role: :buyer)}
+  let(:seller) { create(:user, role: :seller)}
+  let(:store) { create(:store, user: seller)}
 
   let(:different_store) do
-    Store.create!(name: '94 cafe', user: seller)
+    Store.create!(name: '94 cafe', user: seller, category: Store.categories.keys.sample)
   end
 
   let(:order) do
@@ -40,9 +20,14 @@ RSpec.describe OrderItem, type: :model do
   end
 
   describe 'validations' do
+    it { should validate_presence_of(:amount)}
+    it { should validate_presence_of(:price)}
+    it { should validate_numericality_of(:amount).is_greater_than(0)}
+    it { should validate_numericality_of(:price).is_greater_than(0)}
+
     context 'when product belongs to the same store' do
       it 'should be valid' do
-        product = Product.create(store:, title: 'Burger', price: 25.00)
+        product = create(:product, store:)
         order_item = OrderItem.create(order:, product:, price: product.price)
         expect(order_item).to be_valid
       end
@@ -50,12 +35,21 @@ RSpec.describe OrderItem, type: :model do
 
     context 'when product belongs to a different store' do
       it 'should be invalid' do
-        product = Product.create(store: different_store, title: 'Burger', price: 29.00)
+        product = create(:product, store: different_store)
         order_item = OrderItem.create(order:, product:, price: product.price)
 
         expect(order_item).to_not be_valid
         expect(order_item.errors.full_messages).to eq ["Product should belong to `Store`: #{order.store.name}"]
       end
+    end
+  end
+
+  describe '#total_price' do
+    let(:product) {create(:product, store:)}
+    let(:order_item) {OrderItem.create!(amount: 2, price: product.price, order:, product:)}
+
+    it 'returns the product of the amount and the price' do
+      expect(order_item.total_price).to eq(order_item.price * order_item.amount)
     end
   end
 end
