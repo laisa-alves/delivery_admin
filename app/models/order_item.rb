326f7d199fa715bb88c3f5f3e1a364af1f09e535
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-06-06 12:08:57 -0300

    Adiciona validação para order_item e o método para calcular o preço total do item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,6 +3,12 @@ class OrderItem < ApplicationRecord
   belongs_to :product
 
   validate :store_product
+  validates :amount, numericality: { greater_than: 0 }, presence: true
+  validates :price, numericality: { greater_than: 0 }, presence: true
+
+  def total_price
+    amount * price
+  end
 
   private
   def store_product
